13d3f4d5d20ba6b5862923da28234edc0af3b6f5
adrian mysc <61851017+adrian-mysc@users.noreply.github.com>
2026-05-13 22:22:29 -0400

    Change SUPABASE_ANON_KEY in config.js
    Updated SUPABASE_ANON_KEY to a new value.

--- js/supabase/config.js
+++ js/supabase/config.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 const SUPABASE_URL = 'https://iizzeeceqysdfhnkosrc.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_X1_nQ6m48h-w7atI8fhECg_Gj9kXQyl';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpenplZWNlcXlzZGZobmtvc3JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3MTM4MDYsImV4cCI6MjA5NDI4OTgwNn0.tdoZWMwPoWz6uxmj1-6QNrYU8tt1u7mF8cGUY0DHHho';
 
 export const supabase = createClient(
   SUPABASE_URL,
